2b27856c23d6109a708f3f4e64a53a40deb92bc9
Kaustubh <132165310+kaustubh-aras@users.noreply.github.com>
2024-08-12 20:47:03 +0530

    Added Button Component

---
+++ nurturing_disc/components/buttons/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FaArrowRightLong } from "react-icons/fa6";
+
+const Button = ({ children }) => {
+  return (
+    <button className="bg-[#F39F5F] flex flex-row items-center justify-center px-10 py-5 rounded-3xl gap-2 text-white ml-8 font-bold">
+      {children}
+      <FaArrowRightLong />
+    </button>
+  );
+};
+
+export default Button;
