b8af58ba503cb7ed0881b49fafbb92c810be2dc6
Kaustubh <132165310+kaustubh-aras@users.noreply.github.com>
2024-08-14 19:44:19 +0530

    Add extra div to fix selection bug

--- nurturing_disc/app/home/ProgramSection.jsx
+++ nurturing_disc/app/home/ProgramSection.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 const ProgramSection = () => {
   return (
-    <div className="flex flex-col items-center container mx-auto px-4 md:px-6 lg:px-8 relative">
+    <div className="flex flex-col items-center container mx-auto px-4 md:px-6 lg:px-8 relative pt-20 md:pt-28">
       <div className="hidden md:flex">
         <Image
           src="/cartoons/mask.svg"
@@ -33,11 +33,13 @@ const ProgramSection = () => {
           className="absolute top-10 right-12"
         />
       </div>
-      <div className="pt-20 md:pt-28 font-sans font-semibold text-xl text-peach text-center">
-        {programSectionData.title}
-      </div>
-      <div className="font-quicksand text-fiord font-bold text-4xl md:text-5xl max-w-2xl text-center pt-6 md:pt-10">
-        {programSectionData.subHeading}
+      <div>
+        <div className="font-sans font-semibold text-xl text-peach text-center">
+          {programSectionData.title}
+        </div>
+        <div className="font-quicksand text-fiord font-bold text-4xl md:text-5xl max-w-2xl text-center pt-6 md:pt-10">
+          {programSectionData.subHeading}
+        </div>
       </div>
       {/* //Card Section */}
       <div className="pt-10 md:pt-16 pb-16 md:pb-24 flex flex-col md:flex-row gap-8 md:gap-10 lg:gap-12 md:px-4">
