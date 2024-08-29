b26046f0437f20157c69c50b64e04b35ec20e2e1
Kaustubh <132165310+kaustubh-aras@users.noreply.github.com>
2024-08-29 19:07:50 +0530

    Added Container in Home Activities

--- nurturing_disc/app/home/AboutUsSection.jsx
+++ nurturing_disc/app/home/AboutUsSection.jsx
@@ -22,62 +22,64 @@ const AboutUsSection = () => {
 
   return (
     <div className="flex flex-col md:flex-row justify-around relative p-4 md:p-8 bg-[#EFF5F6]">
-      <Image
-        src="/cartoons/bus.svg"
-        width={100}
-        height={100}
-        alt="bus"
-        className="absolute top-10 left-4 md:top-16 md:left-7"
-      />
-      <div className="left py-16 md:py-32 pl-9">
+      <div className="container mx-auto flex flex-row">
         <Image
-          style={{
-            WebkitMaskImage: `url(${homeAboutUsImageData})`,
-            WebkitMaskRepeat: "no-repeat",
-            WebkitMaskSize: "cover",
-            WebkitMaskPosition: "center",
-          }}
-          src="/home/aboutUsAssets/aboutus-image.svg"
-          width={600}
-          height={400}
-          alt="About Us Image"
+          src="/cartoons/bus.svg"
+          width={100}
+          height={100}
+          alt="bus"
+          className="absolute top-10 left-4 md:top-16 md:left-7"
         />
-      </div>
-      <div className="right max-w-3xl py-16 md:py-32">
-        <Image
-          src="/cartoons/girl.svg"
-          width={150}
-          height={150}
-          alt="girl"
-          className="absolute top-56 right-4 md:top-80 md:right-7 flex-shrink-0"
-        />
-        <div className="font-sans font-semibold text-xl text-[#F39F5F] pb-3 text-center md:text-start">
-          {LandingAboutUs.AboutUsSection.title}
+        <div className="left py-16 md:py-32 pl-9">
+          <Image
+            style={{
+              WebkitMaskImage: `url(${homeAboutUsImageData})`,
+              WebkitMaskRepeat: "no-repeat",
+              WebkitMaskSize: "cover",
+              WebkitMaskPosition: "center",
+            }}
+            src="/home/aboutUsAssets/aboutus-image.svg"
+            width={600}
+            height={400}
+            alt="About Us Image"
+          />
         </div>
-        <div className="font-quicksand font-bold text-3xl md:text-5xl text-fiord pb-7">
-          {LandingAboutUs.AboutUsSection.heading}
-        </div>
-        <div className="font-sans font-normal text-base text-fiord pb-10">
-          {LandingAboutUs.AboutUsSection.description}
-        </div>
-        <div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-            {data.map((item) => (
-              <AboutUsInfo key={item.id} title={item.title} />
-            ))}
+        <div className="right max-w-3xl py-16 md:py-32">
+          <Image
+            src="/cartoons/girl.svg"
+            width={150}
+            height={150}
+            alt="girl"
+            className="absolute top-56 right-4 md:top-80 md:right-7 flex-shrink-0"
+          />
+          <div className="font-sans font-semibold text-xl text-[#F39F5F] pb-3 text-center md:text-start">
+            {LandingAboutUs.AboutUsSection.title}
           </div>
-        </div>
-        <div className="flex flex-col md:flex-row items-center pt-10">
-          <div className="flex flex-row items-center gap-6">
-            <Link href={"/aboutus"}>
-              <Button>Explore More</Button>
-            </Link>
+          <div className="font-quicksand font-bold text-3xl md:text-5xl text-fiord pb-7">
+            {LandingAboutUs.AboutUsSection.heading}
           </div>
-          <div className="pt-5 md:pt-0 md:pl-7 flex flex-row items-center gap-6 font-quicksand font-semibold text-sm text-fiord text-center md:text-left">
-            <PhoneIcon />
-            <div>
-              <p>{LandingAboutUs.AboutUsSection.contactInfo}</p>
-              <p>{LandingAboutUs.AboutUsSection.contactNumber}</p>
+          <div className="font-sans font-normal text-base text-fiord pb-10">
+            {LandingAboutUs.AboutUsSection.description}
+          </div>
+          <div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+              {data.map((item) => (
+                <AboutUsInfo key={item.id} title={item.title} />
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row items-center pt-10">
+            <div className="flex flex-row items-center gap-6">
+              <Link href={"/aboutus"}>
+                <Button>Explore More</Button>
+              </Link>
+            </div>
+            <div className="pt-5 md:pt-0 md:pl-7 flex flex-row items-center gap-6 font-quicksand font-semibold text-sm text-fiord text-center md:text-left">
+              <PhoneIcon />
+              <div>
+                <p>{LandingAboutUs.AboutUsSection.contactInfo}</p>
+                <p>{LandingAboutUs.AboutUsSection.contactNumber}</p>
+              </div>
             </div>
           </div>
         </div>
