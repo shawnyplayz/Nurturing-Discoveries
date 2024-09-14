d1bcb96e8459cb4ee6ea79de1bbbdba922d8b76a
Kaustubh <132165310+kaustubh-aras@users.noreply.github.com>
2024-09-15 00:32:55 +0530

    Bug Fix

--- nurturing_disc/components/cards/ActivityCard.jsx
+++ nurturing_disc/components/cards/ActivityCard.jsx
@@ -8,15 +8,15 @@ const ActivityCard = ({ title, description, backgroundColor, src }) => {
         className="flex items-center justify-center p-4 sm:p-5 rounded-lg"
         style={{ backgroundColor }}
       >
-        <div className="relative md:w-12 md:h-12 ">
+        <div className="relative w-16 h-16 sm:w-12 sm:h-12">
           <Image src={src} layout="fill" objectFit="cover" alt="Photo" />
         </div>
       </div>
       <div className="flex flex-col justify-center pl-0 sm:pl-5 mt-2 sm:mt-0">
-        <div className="pb-1 font-quicksand font-bold text-lg sm:text-xl">
+        <div className="pb-1 font-quicksand font-bold text-base sm:text-xl">
           {title}
         </div>
-        <div className="font-sans font-normal text-base text-fiord sm:text-lg px-5 md:px-0">
+        <div className="font-sans font-normal text-sm sm:text-lg text-fiord px-3 sm:px-0">
           {description}
         </div>
       </div>
